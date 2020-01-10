c0fb8c6eb01b2abe58d049c2d47714fee24cd876
fsociety <davidnguyen220593@gmail.com>
2020-01-11 05:04:58 +0700

    update

--- second_install.sh
+++ second_install.sh
@@ -14,14 +14,14 @@
 # ./odoo-install
 ################################################################################
 
-OE_USER="odoo"
+OE_USER="odoo2"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 # The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 # Set to true if you want to install it, false if you don't need it or have it already installed.
-INSTALL_WKHTMLTOPDF="True"
+INSTALL_WKHTMLTOPDF="False"
 # Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8069"
+OE_PORT="8070"
 # Choose the Odoo version which you want to install. For example: 13.0, 12.0, 11.0 or saas-18. When using 'master' the master version will be installed.
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 13.0
 OE_VERSION="13.0"
@@ -55,8 +55,8 @@ sudo apt-get upgrade -y
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
-echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql postgresql-server-dev-all -y
+# echo -e "\n---- Install PostgreSQL Server ----"
+# sudo apt-get install postgresql postgresql-server-dev-all -y
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
@@ -64,35 +64,34 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
+# echo -e "\n--- Installing Python 3 + pip3 --"
+# sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
 
-echo -e "\n---- Install python packages/requirements ----"
-sudo pip3 install psycogreen
-sudo pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+# echo -e "\n---- Install python packages/requirements ----"
+# sudo pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
 
-echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
-sudo apt-get install nodejs npm -y
-sudo npm install -g rtlcss
+# echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
+# sudo apt-get install nodejs npm -y
+# sudo npm install -g rtlcss
 
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
-if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
-  echo -e "\n---- Install wkhtml and place shortcuts on correct place for ODOO 13 ----"
-  #pick up correct one from x64 & x32 versions:
-  if [ "`getconf LONG_BIT`" == "64" ];then
-      _url=$WKHTMLTOX_X64
-  else
-      _url=$WKHTMLTOX_X32
-  fi
-  sudo wget $_url
-  sudo gdebi --n `basename $_url`
-  sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
-  sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
-else
-  echo "Wkhtmltopdf isn't installed due to the choice of the user!"
-fi
+# if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
+#   echo -e "\n---- Install wkhtml and place shortcuts on correct place for ODOO 13 ----"
+#   #pick up correct one from x64 & x32 versions:
+#   if [ "`getconf LONG_BIT`" == "64" ];then
+#       _url=$WKHTMLTOX_X64
+#   else
+#       _url=$WKHTMLTOX_X32
+#   fi
+#   sudo wget $_url
+#   sudo gdebi --n `basename $_url`
+#   sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
+#   sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
+# else
+#   echo "Wkhtmltopdf isn't installed due to the choice of the user!"
+# fi
 
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
@@ -153,6 +152,12 @@ fi
 sudo su root -c "printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> /etc/${OE_CONFIG}.conf"
 sudo su root -c "printf 'xmlrpc_port = ${OE_PORT}\n' >> /etc/${OE_CONFIG}.conf"
 sudo su root -c "printf 'logfile = /var/log/${OE_USER}/${OE_CONFIG}.log\n' >> /etc/${OE_CONFIG}.conf"
+
+# Optimize Odoo performance with enable multiprocessing mode for 2 cores CPU, 4GB RAM
+sudo su root -c "printf 'workers = 5\n' >> /etc/${OE_CONFIG}.conf"
+sudo su root -c "printf 'limit_memory_hard = 3840\n' >> /etc/${OE_CONFIG}.conf"
+sudo su root -c "printf 'limit_memory_soft = 3200\n' >> /etc/${OE_CONFIG}.conf"
+
 if [ $IS_ENTERPRISE = "True" ]; then
     sudo su root -c "printf 'addons_path=${OE_HOME}/enterprise/addons,${OE_HOME_EXT}/addons\n' >> /etc/${OE_CONFIG}.conf"
 else
